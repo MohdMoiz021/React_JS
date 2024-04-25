cce9a855a6ddb8b3f170e85be82d337e19bffbae
Mohd Moiz <mohdmoiz.3287@gmail.com>
2024-04-25 21:48:29 +0530

    Loader Error Handling

---
+++ src/common/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div className="bg-red-600 m-3 text-white rounded p-2">
+    Something went wrong, please try again
+    </div>
+  )
+}
+
+export default Error
+
--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -1,40 +1,34 @@
 import React from "react";
 import axios from 'axios'
 import UserItem from "./UserItem";
+import Loader from "../common/Loader";
+import Error from "../common/Error";
 
 // library : Framework it uses axios for fetching the data axios is a package it helps u to make an api call
 class Users extends React.Component{
     state={
         users:[],
-        hasError:false
+        hasError:false,
+        loading:true,
     }
 
     constructor(){ 
     super()
     axios.get('https://api.github.com/users')
-    .then(res=>this.setState({users:res.data}))
-    .catch(err=>{
-        this.setState({hasError:true})
-    });
+    .then(res=>this.setState({users:res.data,loading:false}))
+    .catch(err=>{this.setState({hasError:true})})
+    .finally(()=>this.setState({loading:false}))
 }
     
- render(){
-    
-    return<div>
-    {
-        this.state.hasError ? <div className="bg-red-600 m-3 text-white rounded p-2">
-        Something went wrong, please try again
-        </div> : null
-    }
-    
-    <h1 className="font-bold ml-5">Users</h1>;
-    { !this.setState.hasError? 
-        this.state.users.map(user=> <UserItem user={user}/>
-      
-    )  : null}
+ render() { 
+    return (
+    <div>
+    {this.state.hasError ? <Error /> : null}
+     <h1 className="font-bold ml-5">Users</h1>
+    {this.state.loading ? <Loader/> :null} 
+    { !this.setState.hasError ? this.state.users.map(user=> <UserItem user={user}/>) :null}
     </div>
- }
-}
+)}}
 
 export default Users;
 
